acaf249b2879e186e07012b06b3237ecdc7d6118
Михайло Саввін <savvin@dlit.dp.ua>
2018-10-18 11:52:01 +0300

    WIP: create IRepository & IUnitOfWork + test 'BookRepository'

---
+++ TMS.Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using TMS.Entities;
+
+namespace TMS.Interfaces
+{
+   public interface IUnitOfWork
+    {
+        IRepository<Book> Books { get; }
+
+        void Save();
+    }
+}
