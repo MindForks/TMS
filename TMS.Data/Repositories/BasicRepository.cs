a7d77a428dc9c544743cd1d41f9627c3a373b24a
Михайло Саввін <savvin@dlit.dp.ua>
2018-10-21 13:54:51 +0300

    WIP: create Entity "NotificationType" + create InitialMigration

---
+++ TMS.Data/Repositories/BasicRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using TMS.Interfaces;
+
+namespace TMS.Data.Repositories
+{
+    public class BasicRepository<T> : IRepository<T>
+        where T : class
+    {
+        private readonly DbSet<T> db;
+
+        public BasicRepository(TMSDbContext context)
+        {
+            db = context.Set<T>();
+        }
+        public void Create(T item)
+        {
+            db.Add(item);
+        }
+
+        public void Delete(int id)
+        {
+            var item = db.Find(id);
+            if (item != null)
+                db.Remove(item);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return db;
+        }
+
+        public IEnumerable<T> Filter(Expression<Func<T, bool>> predicate)
+        {
+            return db.Where(predicate);
+        }
+
+        public T GetItem(int id)
+        {
+            return db.Find(id);
+        }
+
+        public void Update(T item)
+        {
+            db.Update(item);
+        }
+    }
+}
