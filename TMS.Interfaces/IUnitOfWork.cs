e86d99c7491d21e0fe93179139a1546c858afaad
Михайло Саввін <savvin@dlit.dp.ua>
2018-10-21 14:25:57 +0300

    WIP: fix repository

--- TMS.Interfaces/IUnitOfWork.cs
+++ TMS.Interfaces/IUnitOfWork.cs
@@ -1,8 +1,10 @@
-﻿namespace TMS.Interfaces
+﻿using TMS.Entities;
+namespace TMS.Interfaces
 {
    public interface IUnitOfWork
     {
         #region IRepositories
+        IRepository<NotificationType> NotificationTypes { get; }
         #endregion
         void Save();
     }
