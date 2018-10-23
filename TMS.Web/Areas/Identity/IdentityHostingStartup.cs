f1c0257c3ba4d728a13800cad912a2693d4ee3af
OlgaLisniak <olgalisniak24@gmail.com>
2018-10-24 01:41:48 +0300

    Add Simple Start Page

---
+++ TMS.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TMS.Web.Data;
+
+[assembly: HostingStartup(typeof(TMS.Web.Areas.Identity.IdentityHostingStartup))]
+namespace TMS.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
